cad458b083d41fc7cb4b1c3fd149fa22b6380444
Kztech <pathmaker.525@gmail.com>
2021-07-30 08:28:21 +0300

    Update presale contract

--- Contracts/OpenZeppelin-Presale.sol
+++ Contracts/OpenZeppelin-Presale.sol
@@ -35,7 +35,6 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
-
 interface IUniswapV2Factory {
 
     event PairCreated(address indexed token0, address indexed token1, address pair, uint);
