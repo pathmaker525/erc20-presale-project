abe4d0a76e1bfa3c0c18d92a6d82d8cd08720afa
Kztech <pathmaker.525@gmail.com>
2021-07-30 16:51:11 +0300

    Modify ascii art

--- test/presale_test.js
+++ test/presale_test.js
@@ -1,21 +1,21 @@
-// const RE_Presale = artifacts.require("RE_Presale")
-// const RE_Token = artifacts.require("RE_Token")
+const RE_Presale = artifacts.require("RE_Presale")
+const RE_Token = artifacts.require("RE_Token")
 
-// const chai = require("./setupchai.js")
-// const BN = web3.utils.BN
-// const expect = chai.expect
+const chai = require("./setupchai.js")
+const BN = web3.utils.BN
+const expect = chai.expect
 
-// require("dotenv").config({ path: "./.env" })
+require("dotenv").config({ path: "./.env" })
 
-// contract("Real Estate Token Presale Test", async (accounts) => {
-//   const [deployerAccount, recipient, anotherAccount] = accounts
+contract("Real Estate Token Presale Test", async (accounts) => {
+  const [deployerAccount, recipient] = accounts
 
-//   beforeEach(async () => {
-//     console.log(deployerAccount)
-//     console.log(recipient)
-//     this.testToken = await RE_Token.new()
-//     this.testPresale = await RE_Presale.new()
-//   })
+  beforeEach(async () => {
+    console.log(deployerAccount)
+    console.log(recipient)
+    this.testToken = await RE_Token.new()
+    this.testPresale = await RE_Presale.new()
+  })
 
-//   it("")
-// })
+  it("")
+})
