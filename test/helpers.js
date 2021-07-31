d93232e6f77b62efe80e7cef3c9dd4e027282dda
Kztech <pathmaker.525@gmail.com>
2021-07-31 09:09:20 +0300

    Update project

--- test/helpers.js
+++ test/helpers.js
@@ -1,194 +1,168 @@
-const waitUntilTransactionsMined = (txn_hashes) => {
-    var transactionReceiptAsync;
-    const interval = 500;
-    transactionReceiptAsync = function(txn_hashes, resolve, reject) {
-        try {
-            var receipt = web3.eth.getTransactionReceipt(txn_hashes);
-            if (receipt == null) {
-                setTimeout(function () {
-                    transactionReceiptAsync(txn_hashes, resolve, reject);
-                }, interval);
-            } else {
-                resolve(receipt);
-            }
-        } catch(e) {
-            reject(e);
-        }
-    };
+const { expect } = require("./setupchai.js")
 
-    if (Array.isArray(txn_hashes)) {
-        var promises = [];
-        txn_hashes.forEach(function (tx_hash) {
-            promises.push(waitUntilTransactionsMined(tx_hash));
-        });
-        return Promise.all(promises);
+const waitUntilTransactionsMined = (txn_hashes) => {
+  var transactionReceiptAsync
+  const interval = 500
+  transactionReceiptAsync = function (txn_hashes, resolve, reject) {
+    try {
+      var receipt = web3.eth.getTransactionReceipt(txn_hashes)
+      if (receipt == null) {
+        setTimeout(function () {
+          transactionReceiptAsync(txn_hashes, resolve, reject)
+        }, interval)
+      } else {
+        resolve(receipt)
+      }
+    } catch (e) {
+      reject(e)
     }
-    else {
-        return new Promise(function (resolve, reject) {transactionReceiptAsync(txn_hashes, resolve, reject);});
-    }
+  }
+
+  if (Array.isArray(txn_hashes)) {
+    var promises = []
+    txn_hashes.forEach(function (tx_hash) {
+      promises.push(waitUntilTransactionsMined(tx_hash))
+    })
+    return Promise.all(promises)
+  } else {
+    return new Promise(function (resolve, reject) {
+      transactionReceiptAsync(txn_hashes, resolve, reject)
+    })
+  }
 }
 
 const transferOwnership = async (crowdsale, sender, receiver) => {
-    tx = await crowdsale.transferOwnership(receiver, {from: sender, gas: 100000});
-    await waitUntilTransactionsMined(tx.tx);
-
-}
-
-const setContactInformation = async(crowdsale, sender, message) => {
-    tx = await crowdsale.setContactInformation(message, {from: sender, gas: 1000000});
-    await waitUntilTransactionsMined(tx.tx);
-}
-
-const getContactInformation = async(crowdsale, sender) => {
-    return await crowdsale.contactInformation.call();
-}
-
-const pause = async(contract, sender) => {
-    tx = await contract.pause({from: sender, gas: 1000000});
-    await waitUntilTransactionsMined(tx.tx);
-}
-
-const unpause = async(contract, sender) => {
-    tx = await contract.unpause({from :sender, gas: 1000000});
-    await waitUntilTransactionsMined(tx.tx);
+  tx = await crowdsale.transferOwnership(receiver, {
+    from: sender,
+    gas: 100000,
+  })
+  await waitUntilTransactionsMined(tx.tx)
 }
 
 const expectInvalidOpcode = async (promise) => {
-    try {
-        await promise;
-    }
-    catch (error) {
-        expect(error.message).to.include('invalid opcode')
-        return;
-    }
-    expect.fail('Expected throw not received');
+  try {
+    await promise
+  } catch (error) {
+    expect(error.message).to.include("invalid opcode")
+    return
+  }
+  expect.fail("Expected throw not received")
 }
 
 const expectInvalidJump = async (promise) => {
-    try {
-        await promise;
-    }
-    catch (error) {
-        expect(error.message).to.include('invalid JUMP');
-        return;
-    }
-    expect.fail('Expected throw not received')
+  try {
+    await promise
+  } catch (error) {
+    expect(error.message).to.include("invalid JUMP")
+    return
+  }
+  expect.fail("Expected throw not received")
 }
 
 const expectOutOfGas = async (promise) => {
-    try {
-        await promise;
-    }
-    catch (error) {
-        expect(error.message).to.include('out of gas');
-        return;
-    }
-    expect.fail('Expected throw not received');
+  try {
+    await promise
+  } catch (error) {
+    expect(error.message).to.include("out of gas")
+    return
+  }
+  expect.fail("Expected throw not received")
 }
 
 const ether = (amount) => {
-    return new web3.BigNumber(web3.toWei(amount, 'ether')).toString();
+  return new web3.utils.BN(web3.utils.toWei(amount.toString(), "ether"))
 }
 
 const makeTransaction = async (tx_obj) => {
-    let tx_hash = await web3.eth.sendTransaction(tx_obj)
-    return tx_hash
+  let tx_hash = await web3.eth.sendTransaction(tx_obj)
+  return tx_hash
 }
 
 const makeTransactions = async (txns) => {
+  let txn_hashes = []
 
-    let txn_hashes = [];
+  for (let txn of txns) {
+    txn_hash = await makeTransaction(txn)
+    txn_hashes.push(txn_hash)
+  }
 
-    for (let txn of txns) {
-        txn_hash = await makeTransaction(txn);
-        txn_hashes.push(txn_hash);
-    }
-
-    return txn_hashes;
+  return txn_hashes
 }
 
-
 const getTotalRaised = async (crowdsale) => {
-    let totalRaised = await crowdsale.weiRaised.call();
-    console.log(`The total amount of ether raised is ${inEther(totalRaised)}`);
+  let totalRaised = await crowdsale.weiRaised.call()
+  console.log(`The total amount of ether raised is ${inEther(totalRaised)}`)
 }
 
 const getTokenTotalSupply = async (token) => {
-    let totalSupply = await token.totalSupply.call();
-    return inEther(totalSupply.toString());
+  let totalSupply = await token.totalSupply.call()
+  return inEther(totalSupply.toString())
 }
 
 const getTokenBalance = async (token, address) => {
-    let balance = await token.balanceOf(address);
-    return balance;
+  let balance = await token.balanceOf(address)
+  return balance
 }
 
-const getTokenDistribution = async(token, investors) => {
+const getTokenDistribution = async (token, investors) => {
+  const promises = investors.map(async (address) => {
+    return { address: address, balance: await getTokenBalance(token, address) }
+  })
 
-    const promises = investors.map(async (address) => {
-        return { address: address, balance: await getTokenBalance(token, address) };
-    });
-
-    return Promise.all(promises);
+  return Promise.all(promises)
 }
 
 const getBalance = (address) => {
-    return web3.fromWei(web3.eth.getBalance(address).toString(), 'ether');
+  return web3.fromWei(web3.eth.getBalance(address).toString(), "ether")
 }
 
 const inEther = (amountInWei) => {
-    return web3.fromWei(amountInWei, 'ether');
+  return web3.fromWei(amountInWei, "ether")
 }
 
 const inWei = (amountInEther) => {
-    return web3.toWei(amountInEther, 'ether');
+  return web3.toWei(amountInEther, "ether")
 }
 
 const tokenWei = (amountInTokens) => {
-    return amountInTokens * (10 ** 18);
+  return amountInTokens * 10 ** 18
 }
 
 const inBaseUnits = (tokens) => {
-    return tokens * (10 ** 18);
+  return tokens * 10 ** 18
 }
 
 const inTokenUnits = (tokenBaseUnits) => {
-    return tokenBaseUnits / (10 * 18);
+  return tokenBaseUnits / (10 * 18)
 }
 
 const equivalentTokenBaseUnits = (wei) => {
-    let rate = 20;
-    return rate * wei;
+  let rate = 20
+  return rate * wei
 }
 
 const equivalentTokenUnits = (wei) => {
-    let rate = 20;
-    return rate * wei / (10 ** 18);
+  let rate = 20
+  return (rate * wei) / 10 ** 18
 }
 
-
-
 module.exports = {
-    waitUntilTransactionsMined,
-    transferOwnership,
-    expectInvalidOpcode,
-    expectInvalidJump,
-    expectOutOfGas,
-    setContactInformation,
-    getContactInformation,
-    pause,
-    unpause,
-    ether,
-    getBalance,
-    getTokenBalance,
-    getTotalRaised,
-    makeTransaction,
-    makeTransactions,
-    inEther,
-    inWei,
-    getTokenBalance,
-    equivalentTokenBaseUnits,
-    equivalentTokenUnits,
-    inBaseUnits,
-    inTokenUnits
+  waitUntilTransactionsMined,
+  transferOwnership,
+  expectInvalidOpcode,
+  expectInvalidJump,
+  expectOutOfGas,
+  ether,
+  getBalance,
+  getTokenBalance,
+  getTotalRaised,
+  makeTransaction,
+  makeTransactions,
+  inEther,
+  inWei,
+  getTokenBalance,
+  equivalentTokenBaseUnits,
+  equivalentTokenUnits,
+  inBaseUnits,
+  inTokenUnits,
 }
--- test/project_test.js
+++ test/project_test.js
@@ -1,84 +1,105 @@
-const RE_Token = artifacts.require("RE_Token")
-const RE_PreSale = artifacts.require("RE_PreSale")
+const Token = artifacts.require("RE_Token")
+const PreSale = artifacts.require("RE_PreSale")
 
-const chai = require("./setupchai.js")
+const { expect, assert } = require("./setupchai.js")
 const BN = web3.utils.BN
-const expect = chai.expect
 
-const helper = require("./helpers.js")
-const ether = helper.ether
-const getBalance = helper.getBalance
+const {
+  ether,
+  getBalance,
+  transferOwnership,
+  makeTransaction,
+} = require("./helpers.js")
 
 require("dotenv").config({ path: "./.env" })
 
-contract("Real Estate Token Test", async (accounts) => {
-  const [investor, receiver, hacker] = accounts
-
-  const investment = ether(10)
-  const smallInvestment = ether(0.5)
-  const hugeInvestment = ether(100)
+contract("Real Estate Project Test", async (accounts) => {
+  const [investor, receiver, hacker_1, hacker_2] = accounts
 
   console.log("Investor: ", investor)
   console.log("Receiver: ", receiver)
 
+  const investment = ether(10)
+  const smallInvestment = ether(0.5)
+  const hugeInvestment = ether(100)
+  const softCap = ether(2500)
+  const hardCap = ether(4000)
+  const transfer = new BN(150000000000000)
+
   beforeEach(async () => {
-    this.token = await RE_Token.new()
-    this.presale = await RE_Presale.new(5, investor, this.token.address)
+    this.token = await Token.new()
+    this.presale = await PreSale.new(5, investor, this.token.address)
     this.owner = await this.presale.owner.call()
-    this.wallet = await this.presale.wallet.call()
   })
-  // beforeEach(async () => {
-  //   console.log(deployerAccount)
-  //   console.log(recipient)
-  //   this.token = await RE_Token.new()
-  // })
 
-  // it("All tokens should be in my account", async () => {
-  //   const instance = this.testToken
-  //   const totalSupply = await instance.totalSupply()
+  describe("Ownership", async () => {
+    it("should initially belong to contract caller", async () => {
+      assert.equal(this.owner, investor)
+    })
+    it("should be transferable to another account", async () => {
+      await transferOwnership(this.presale, this.owner, receiver)
+      const newOwner = await this.presale.owner.call()
+      assert.equal(receiver, newOwner)
+    })
+    // it("should not be transferable by non-owner", async () => {
+    //   await expectInvalidOpcode(
+    //     transferOwnership(this.presale, hacker_1, hacker_2)
+    //   )
+    //   const newOwner = await this.presale.owner.call()
+    //   return assert.equal(this.owner, newOwner)
+    // })
+  })
 
-  //   return expect(
-  //     instance.balanceOf(deployerAccount)
-  //   ).to.eventually.be.a.bignumber.equal(totalSupply)
-  // })
+  describe("Tokens", async () => {
+    it("all should initially be in owners account", async () => {
+      await expect(
+        this.token.balanceOf(investor)
+      ).to.eventually.be.a.bignumber.equal(await this.token.totalSupply())
+    })
+    it("should be able to transfer between accounts", async () => {
+      await expect(this.token.transfer(receiver, transfer)).to.eventually.be
+        .fulfilled
+    })
+    it("transfer exceed total amount should be rejected", async () => {
+      await expect(
+        this.token.transfer(
+          receiver,
+          new BN((await this.token.totalSupply()) + 1)
+        )
+      ).to.eventually.be.rejected
+    })
+    it("transfer to or from blacklisted address will be rejected", async () => {
+      await expect(this.token.setAddressAsBlacklisted(hacker_1)).to.eventually
+        .be.fulfilled
+      await expect(this.token.transfer(hacker_1, transfer)).to.eventually.be
+        .rejected
+    })
+  })
 
-  // it("Token transfer between accounts is possible", async () => {
-  //   const sendTokens = 10
-  //   const instance = this.testToken
-  //   const totalSupply = await instance.totalSupply()
+  // describe("Pre-Sale", async () => {
+  //   it("ICO starts", async () => {
+  //     await expect(
+  //       this.presale.startICO(
+  //         1630376078,
+  //         smallInvestment,
+  //         hugeInvestment,
+  //         await this.token.totalSupply(),
+  //         softCap,
+  //         hardCap
+  //       )
+  //     ).to.eventually.be.fulfilled
 
-  //   await expect(
-  //     instance.balanceOf(deployerAccount)
-  //   ).to.eventually.be.a.bignumber.equal(totalSupply)
-  //   await expect(instance.transfer(recipient, new BN(sendTokens))).to.eventually
-  //     .be.fulfilled
-  //   await expect(
-  //     instance.balanceOf(deployerAccount)
-  //   ).to.eventually.be.a.bignumber.equal(totalSupply.sub(new BN(sendTokens)))
-  //   return await expect(
-  //     instance.balanceOf(recipient)
-  //   ).to.eventually.be.a.bignumber.equal(new BN(sendTokens))
-  // })
+  //     txn_obj = {
+  //       from: investor,
+  //       to: this.presale.address,
+  //       value: investment,
+  //       gas: 1000000,
+  //     }
+  //     txn = await expect(makeTransaction(txn_obj)).to.eventually.be.fulfilled
 
-  // it("Token transfer over total amount will be rejected", async () => {
-  //   const instance = this.testToken
-  //   const balanceOfDeployer = await instance.balanceOf(deployerAccount)
-
-  //   expect(instance.transfer(recipient, new BN(balanceOfDeployer + 1))).to
-  //     .eventually.be.rejected
-  //   return expect(
-  //     instance.balanceOf(deployerAccount)
-  //   ).to.eventually.be.a.bignumber.equal(balanceOfDeployer)
-  // })
-
-  // it("Token transfer to or from blacklisted address will be rejected", async () => {
-  //   console.log(botAddress)
-  //   const instance = this.testToken
-  //   const sendTokens = 10
-
-  //   await expect(instance.setAddressAsBlacklisted(botAddress)).to.eventually.be
-  //     .fulfilled
-  //   await expect(instance.transfer(botAddress, new BN(sendTokens))).to
-  //     .eventually.be.rejected
+  //     params = { from: investor, gas: 1000000, value: investment }
+  //     txn_2 = await expect(this.presale.buyTokens(investor, params)).to
+  //       .eventually.be.fulfilled
+  //   })
   // })
 })
